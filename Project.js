d431499196cebfae36049d70a6c21e02c01a77ba
Mridul Kalia <mridulkalia3@gmail.com>
2023-07-10 02:14:20 +0530

    second commit

--- Project.js
+++ Project.js
@@ -7,3 +7,36 @@ Rbutton.addEventListener("click", function () {
 Lbutton.addEventListener("click", function () {
   content.scrollLeft -= 900;
 });
+
+let Sidebar = document.getElementById("sidebar");
+let all = document.getElementById("a1");
+let black_bg = document.querySelector("#black-bg");
+all.addEventListener("click", function () {
+  Sidebar.style.transform = "translateX(0)";
+  Sidebar.style.transition = "200ms";
+  document.body.classList.add("stop-scroll");
+});
+
+let Xmark = document.getElementById("xmark");
+Xmark.addEventListener("click", function () {
+  Sidebar.style.transform = "translateX(-1539px)";
+  document.body.classList.remove("stop-scroll");
+});
+
+let back_to_top = document.getElementById("footer-first");
+back_to_top.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+let sign_out = document.getElementById("sign-out");
+let Lists = document.querySelector(".lists");
+sign_out.addEventListener("mouseover", function () {
+  Lists.style.display = "block";
+  document.body.classList.add("stop-scroll");
+});
+sign_out.addEventListener("mouseout", function () {
+  Lists.style.display = "none";
+  document.body.classList.remove("stop-scroll");
+});
